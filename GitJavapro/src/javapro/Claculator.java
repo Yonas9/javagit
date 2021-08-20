3538e113c774af803e39b241a3c55fe55a15cd09
Yonas Sh <yonakifle@yahoo.com>
2021-08-19 23:27:19 -0700

    java and git

---
+++ GitJavapro/src/javapro/Claculator.java
@@ -0,0 +1,11 @@
+package javapro;
+
+public class Claculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+        System.out.println("leaning Java");
+	}
+
+}
