ea040a437175e6a54b2e1eaf2a0e543e539e12c7
Yonas Sh <yonakifle@yahoo.com>
2021-08-21 04:06:30 -0700

    adder

---
+++ JavaGit/src/Git/Adder.java
@@ -0,0 +1,13 @@
+package Git;
+
+public class Adder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=3,num2=2;
+		int sum=num1+num2;
+		System.out.println(sum);
+
+	}
+
+}
