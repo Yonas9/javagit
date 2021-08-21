6bcd6cbe9156a9091ae7ede93664a45749baff42
Yonas Sh <yonakifle@yahoo.com>
2021-08-21 02:29:11 -0700

    Second pro

---
+++ Helloworld/src/Git/loop.java
@@ -0,0 +1,26 @@
+package Git;
+
+public class loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<5;i++) {
+			
+			for(int j=5-1; j>1;j--)
+			{
+				System.out.print(" ");
+				}
+			for(int k=0;k<=i;k++) 
+			{
+				System.out.print("* ");
+				
+				}
+			System.out.println();
+			
+			
+		}
+	
+
+	}
+}
