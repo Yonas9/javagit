4ce90567c253055931635e694b0424cc304882d2
Yonas Sh <yonakifle@yahoo.com>
2021-08-21 03:54:22 -0700

    it is working

---
+++ JavaGit/src/Git/Remainder.java
@@ -0,0 +1,14 @@
+package Git;
+
+public class Remainder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=7;
+		int num2=2;
+		int remainder = num1 % num2;
+		System.out.println("The remainder of "+ num1+" divided by "+num2+" = "+remainder);
+        System.out.println("");
+	}
+
+}
