b12e08a4b6f0368727e0c268d67aa6a044f6f2d4
Yonas Sh <yonakifle@yahoo.com>
2021-08-21 17:38:05 -0700

    star2

--- StarPyramid/src/Pyramid/Star.java
+++ StarPyramid/src/Pyramid/Star.java
@@ -5,6 +5,8 @@ public class Star {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("Java");
+		System.out.println("Eclipse");
 	}
 
 }
